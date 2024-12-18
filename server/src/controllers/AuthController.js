a9e534eb5dabdc57ad39c3048d7401a590d5d1c7
unknown <bijontalukder1247@gmail.com>
2024-12-18 22:23:43 +0600

    update

---
+++ server/src/controllers/AuthController.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+
+class AuthController {
+    constructor(authService) {
+        this.authService = authService;
+    }
+
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ success: false, message: "Email and password are required" });
+
+            }
+
+            const user = await this.authService.varifyUser(email, password)
+            
+            console.log(user);
+            
+            if (!user) {
+                return res.status(401).json({ success: false, message: "Invalid credentials" });
+            }
+            console.log(user)
+            const token = jwt.sign(
+                { id: user.id, email: user.email, role: user.role },
+                "key123", // Use a secure secret stored in environment variables
+                { expiresIn: "1h" } 
+              );
+              return res.status(200).json({
+                success: true,
+                message: "Login successful",
+                token,
+              });
+
+        } catch (error) {
+            console.error(error)
+            next(error)
+        }
+    }
+}
+
+module.exports =AuthController
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const AdminService = require('../services/Admin/adminService');
 const AdminController = require('../controllers/adminController');
+const AuthService = require('../services/Authentication/AuthService');
+const BcryptHasher = require('../utility/BcryptPasswordHasher');
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
 const prisma = new PrismaClient();
 const adminService = new AdminService(prisma);
 const adminController = new AdminController(adminService);
+const hasher = new BcryptHasher()
+const authService = new AuthService(prisma,hasher)
 
 // Define routes
 router.post('/admin/create', (req, res,next) => adminController.createAdmin(req, res,next));
 
+
+
+//authentication
+router.post('/login',(req,res,next)=>{
+    const authController = new AuthController(authService)
+    authController.login(req,res,next)
+})
 module.exports = router;
