2c54287d5f8f0d5616c30714b530394f709f34ac
ahad30 <mohiminulislamahad@gmail.com>
2025-02-05 00:59:28 +0600

    sddf

--- client/src/Pages/Dashboard/Admin/Orders/EditOrders.jsx
+++ client/src/Pages/Dashboard/Admin/Orders/EditOrders.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React from "react";
+import { toast } from "sonner";
+import { useUpdateOrderMutation } from "../../../../redux/Feature/Admin/order/orderApi";
+import { useAppDispatch } from "../../../../redux/Hook/Hook";
+import ZFormTwo from "../../../../components/Form/ZFormTwo";
+import ZSelect from "../../../../components/Form/ZSelect";
+import { setIsEditModalOpen } from "../../../../redux/Modal/ModalSlice";
 
-const EditOrders = () => {
+const EditOrders = ({ selectedOrder }) => {
+  const dispatch = useAppDispatch();
+  const [editOrder, { isLoading: OIsloading, isError: OIsError, error: OError, isSuccess: OIsSuccess, data }] = useUpdateOrderMutation();
+
+  const handleSubmit = async (formData) => {
+    try {
+      const orderData = {
+        orderStatus: formData?.orderStatus,
+      };
+      
+      await editOrder({ id: selectedOrder.id, data: orderData });
+    } catch (error) {
+      console.error('Error updating order:', error);
+      toast.error('Error updating order. Please try again.');
+    }
+  };
+
+  const handleCloseAndOpen = () => {
+    dispatch(setIsEditModalOpen());
+  };
+
   return (
-    <div>EditOrders</div>
-  )
-}
+    <div className="">
+      <ZFormTwo
+        isLoading={OIsloading}
+        isSuccess={OIsSuccess}
+        isError={OIsError}
+        error={OError}
+        submit={handleSubmit}
+        closeModal={handleCloseAndOpen}  
+        formType="edit"  
+        data={data}
+        buttonName="Update"
+      >
+        <div className="grid grid-cols-1 gap-3 mt-10">
+          <ZSelect
+            name="orderStatus"
+            label="Status"
+            options={[
+              { label: "Pending", value: "pending" },
+              { label: "Processing", value: "processing" },
+              { label: "Shipped", value: "shipped" },
+              { label: "Delivered", value: "delivered" },
+              { label: "Cancelled", value: "cancelled" },
+            ]}
+            placeholder="Select status"
+            value={selectedOrder?.orderStatus}
+          />
+        </div>
+      </ZFormTwo>
+    </div>
+  );
+};
 
-export default EditOrders
+export default EditOrders;
--- client/src/Pages/Dashboard/Admin/Orders/Orders.jsx
+++ client/src/Pages/Dashboard/Admin/Orders/Orders.jsx
@@ -1,9 +1,169 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Table, Tag, Space, Tooltip } from "antd";
+import BreadCrumb from "../../../../components/BreadCrumb/BreadCrumb";
+import DashboardTable from "../../../../components/Table/DashboardTable";
+import ButtonWithModal from "../../../../components/Button/ButtonWithModal";
+import { setIsDeleteModalOpen, setIsEditModalOpen, setIsViewModalOpen } from "../../../../redux/Modal/ModalSlice";
+import EditModal from "../../../../components/Modal/EditModal";
+import DeleteModal from "../../../../components/Modal/DeleteModal";
+import { useAppDispatch, useAppSelector } from "../../../../redux/Hook/Hook";
+import { AiFillDelete, AiFillEdit, AiFillEye } from "react-icons/ai";
+import { useDeleteOrderMutation, useGetOrdersQuery } from "../../../../redux/Feature/Admin/order/orderApi";
+import ViewModal from "../../../../components/Modal/ViewModal";
+import EditOrders from "./EditOrders";
+import ViewOrders from "./ViewOrders";
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const { data, error, isLoading: ordersIsLoading } = useGetOrdersQuery();
+  const { isEditModalOpen, isViewModalOpen, isDeleteModalOpen } = useAppSelector((state) => state.modal);
+  const [selectedOrder, setSelectedOrder] = useState({});
+  const [deleteOrder, { isLoading: dOIsLoading, isError, isSuccess, data: dOData, error: dOError }] = useDeleteOrderMutation();
 
-export default Orders
+  const ordersData = data?.data?.map((order, index) => ({
+    key: index + 1,
+    id: order?.id,
+    name: order?.name,
+    email: order?.email,
+    phone: order?.phone,
+    orderTotal: order?.orderTotal,
+    paymentStatus: order?.paymentStatus,
+    orderStatus: order?.orderStatus,
+    transactionId: order?.transactionId,
+    shippingAddress: order?.shippingAddress,
+    billingAddress: order?.billingAddress,
+    orderItems: order?.orderItems,
+    deliveryFee: order?.deliveryFee,
+    taxAmount: order?.taxAmount
+  }));
+
+  const handleEditOrder = (orderData) => {
+    setSelectedOrder(orderData);
+    dispatch(setIsEditModalOpen());
+  };
+
+  const handleViewOrder = (orderData) => {
+    setSelectedOrder(orderData);
+    dispatch(setIsViewModalOpen());
+  };
+
+  const handleDeleteOrderPrompt = (orderData) => {
+    setSelectedOrder(orderData);
+    dispatch(setIsDeleteModalOpen());
+  };
+
+  const handleDeleteOrder = () => {
+    deleteOrder(selectedOrder?.id);
+  };
+
+  const columns = [
+    {
+      title: "Serial",
+      dataIndex: "key",
+      key: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Order Total",
+      dataIndex: "orderTotal",
+      key: "orderTotal",
+    },
+    {
+      title: "Payment Status",
+      dataIndex: "paymentStatus",
+      key: "paymentStatus",
+      render: (status) => (
+        <Tag color={status === "paid" ? "green" : "red"}>{status}</Tag>
+      ),
+    },
+    {
+      title: "Order Status",
+      dataIndex: "orderStatus",
+      key: "orderStatus",
+      render: (status) => (
+        <Tag 
+        color={
+          status === "pending" ? "blue" :
+          status === "processing" ? "orange" :
+          status === "shipped" ? "purple" :
+          status === "delivered" ? "green" :
+          status === "cancelled" ? "red" :
+          "default"
+        }
+      >
+        {status}
+      </Tag>
+      
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a onClick={() => handleViewOrder(record)}>
+            <Tooltip title="View" placement="top">
+              <AiFillEye className="text-blue-500" size={20} />
+            </Tooltip>
+          </a>
+          <a onClick={() => handleEditOrder(record)}>
+            <Tooltip title="Edit" placement="top">
+              <AiFillEdit className="text-green-500" size={20} />
+            </Tooltip>
+          </a>
+          <a onClick={() => handleDeleteOrderPrompt(record)}>
+            <Tooltip title="Delete" placement="top">
+              <AiFillDelete className="text-red-500" size={20} />
+            </Tooltip>
+          </a>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <BreadCrumb />
+      </div>
+
+      <DashboardTable columns={columns} data={ordersData} loading={ordersIsLoading} />
+
+      <EditModal isEditModalOpen={isEditModalOpen} title="Edit Order">
+        <EditOrders selectedOrder={selectedOrder} />
+      </EditModal>
+      
+      <ViewModal width={600} isViewModalOpen={isViewModalOpen} title="View Order">
+        <ViewOrders selectedOrder={selectedOrder} />
+      </ViewModal>
+      
+      <DeleteModal
+        data={dOData}
+        error={dOError}
+        isLoading={dOIsLoading}
+        isSuccess={isSuccess}
+        title="Delete Order"
+        onDelete={handleDeleteOrder}
+        isDeleteModalOpen={isDeleteModalOpen}
+        isError={isError}
+        description="Deleting this order will remove all associated details permanently."
+      ></DeleteModal>
+    </>
+  );
+};
+
+export default Orders;
