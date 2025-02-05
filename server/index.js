7f4bcf2cc0adb55ce26c84be52a3f4a49010ed08
ahad30 <mohiminulislamahad@gmail.com>
2025-02-06 03:25:35 +0600

    dfdf

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const prisma = new PrismaClient();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://inkspire-steel.vercel.app"
   ],
   credentials: true,
   optionSuccessStatus: 200,
